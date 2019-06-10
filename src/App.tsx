c5c68d87e0815af5780114bf5280ea3a71a44984
wfr112@naver.com <wfr112@naver.com>
2019-06-10 22:42:29 +0900

    update: input 값 들어올 때 validation 사용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const useInput = (initValue: string, validator?: (value: string) => boolean) => {
+  const [value, setValue] = useState<string>(initValue)
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { target: { value } } = event; //event안의 target 속성안의 value를 가져옴
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) setValue(value);
+  }
+  return { value, onChange }
+}
+
 const App = () => {
-  const [count, setCount] = useState(0);
+  const checkEmail = (value: string) => value.includes("@")
+  const name = useInput("Mr. ", checkEmail);
+
   return (
     <>
-    {count}
-    <button onClick={() => setCount(count+1)}>Increment</button>
+      <input placeholder="Name" value={name.value} onChange={name.onChange} />
     </>
   )
 }
