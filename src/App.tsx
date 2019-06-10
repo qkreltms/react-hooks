e9a9f63295290e595c3501362979d157fece3ce1
wfr112@naver.com <wfr112@naver.com>
2019-06-10 22:08:43 +0900

    After hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends React.Component<{}, {}> {
-  state = {
-    count: 0
-  };
-
-  private modify = (n: number) => (e: React.MouseEvent) => {
-    this.setState({
-      count: n
-    });
-  }
-
-  public render() {
-    const { count } = this.state;
-    return (
-      <React.Fragment>
-        <div>{count}</div>
-        <button onClick={this.modify(count+1)}>Increment</button>
-      </React.Fragment>
-    );
-  }
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+    {count}
+    <button onClick={() => setCount(count+1)}>Increment</button>
+    </>
+  )
 }
-
 export default App;
